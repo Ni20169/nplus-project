66f2915c8c1bb1d13c8aaf80b7d3f1224277b82c
Ni20169 <197754846+Ni20169@users.noreply.github.com>
2026-03-13 01:52:10 +0800

    修复迁移文件依赖

--- documents/migrations/0005_projectapproval.py
+++ documents/migrations/0005_projectapproval.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("documents", "0004_remove_projectmaster_org_code"),
+        ("documents", "0001_initial"),
     ]
 
     operations = [
